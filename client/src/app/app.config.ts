9fe7054893f914162c6e874838909c004156f1bc
shellygupta21 <guptashelly2101@gmail.com>
2025-11-02 01:04:46 +0530

    End of section 7

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,15 +2,16 @@ import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDete
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { InitServiceService } from '../core/services/init-service.service';
 import { lastValueFrom } from 'rxjs';
+import { errorInterceptor } from '../core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAppInitializer(async () => {
       const initService = inject(InitServiceService);
 
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailedComponent } from '../features/members/member-detailed/mem
 import { ListsComponent } from '../features/lists/lists.component';
 import { MessagesComponent } from '../features/messages/messages.component';
 import { authGuard } from '../core/guards/auth.guard';
+import { TestErrorsComponent } from '../features/test-errors/test-errors.component';
+import { NotFoundComponent } from '../shared/errors/not-found/not-found.component';
+import { ServerErrorComponent } from '../shared/errors/server-error/server-error.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
@@ -19,6 +22,8 @@ export const routes: Routes = [
             {path:'messages', component: MessagesComponent},
         ]
     },
-    {path:'**', component: HomeComponent},
+    {path: 'errors', component: TestErrorsComponent},
+    {path: 'server-error', component: ServerErrorComponent},
+    {path:'**', component: NotFoundComponent},
 
 ];
