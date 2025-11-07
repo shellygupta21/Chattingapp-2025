1c0ce31a5b42c728fd39081c4d97157d8a0f1fd4
shellygupta21 <guptashelly2101@gmail.com>
2025-11-07 15:01:07 +0530

    End of Section 8

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -7,5 +7,11 @@ namespace API.Data;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+
+    //Nav property
+
+    public Member Member { get; set; } = null!;
 }
 
